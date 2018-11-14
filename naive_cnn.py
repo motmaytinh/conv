b04e241e297933b40342af14e890d2d2cdf539da
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-11-14 14:46:04 +0700

    Bug in layers conv forward

--- naive_cnn.py
+++ naive_cnn.py
@@ -134,31 +134,46 @@ class Model(object):
 def main():
     # X = np.random.randn(100, 3, 32, 32) * 100
 
-    X = np.random.randn(100, 1, 28, 28)
+    X = np.random.randn(10, 1, 28, 28)
 
-    y = np.random.choice(9, 100)
+    y = np.random.choice(9, 10)
 
     model = Model()
 
     # Conv
-    model.add(Conv2D(filters=1, in_channel = 1, kernel_size=2, stride=2, padding=1))
+    model.add(Conv2D(filters=32, in_channel = 1, kernel_size=5, stride=1, padding=1))
 
     # ReLU
-    model.add(ReLU())
+    # model.add(ReLU())
 
     # MaxPool
-    model.add(MaxPooling(pool_size=2, stride=1))
+    # model.add(MaxPooling(pool_size=2, stride=1))
 
     # DropOut
-    model.add(Dropout(0.3))
+    # model.add(Dropout(0.3))
+
+    # Conv
+    # model.add(Conv2D(filters=32, in_channel = 1, kernel_size=2, stride=1, padding=1))
 
     # ReLU
-    model.add(ReLU())
+    # model.add(ReLU())
+
+    # MaxPool
+    # model.add(MaxPooling(pool_size=2, stride=1))
+
+    # DropOut
+    # model.add(Dropout(0.3))
 
     # FC
-    model.add(FullyConnected(hidden_dim=196, num_classes=10))
+    # model.add(FullyConnected(hidden_dim=100, num_classes=100))
 
-    model.fit(X, y, 3, 100)
+    # DropOut
+    # model.add(Dropout(0.3))
+
+    # FC
+    # model.add(FullyConnected(hidden_dim=49, num_classes=10))
+
+    model.fit(X, y, 2, 5)
 
     print(model.predict(np.random.randn(10, 1, 28, 28)))
 
