Fix softmax function

naive_cnn.py
@@ -180,7 +180,7 @@ def main():
     # FC
     model.add(FullyConnected(hidden_dim=1024, num_classes=10))
 
-    model.fit(X, y, 1, 10)
+    model.fit(X, y, 3, 10)
 
     print(model.evaluate(np.random.randn(10, 1, 28, 28), np.random.choice(9, 10)))
 

src_CNN/layers.py
@@ -195,6 +195,5 @@ def dropout_backward(dout, cache):
 def softmax(x):
     shifted_logits = x - np.max(x, axis=1, keepdims=True)
     Z = np.sum(np.exp(shifted_logits), axis=1, keepdims=True)
-    log_probs = shifted_logits - np.log(Z)
-    probs = np.exp(log_probs)
+    probs = shifted_logits/Z
     return probs