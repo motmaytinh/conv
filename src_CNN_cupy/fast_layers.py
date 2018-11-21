6adef06a5d88f1bd616625aa6c1c814d63c6ef3c
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-11-21 08:45:36 +0700

    Update cupy

--- src_CNN_cupy/fast_layers.py
+++ src_CNN_cupy/fast_layers.py
@@ -1,126 +1,76 @@
 import cupy as np
-from src_CNN_cupy.im2col import *
+from src_CNN.im2col import *
 
 
-def conv_forward_im2col(x, w, b, conv_param):
+def conv_forward_fast(x, w, b, conv_param):
     """
     A fast implementation of the forward pass for a convolutional layer
     based on im2col and col2im.
     """
-    N, C, H, W = x.shape
-    num_filters, _, filter_height, filter_width = w.shape
-    stride, pad = conv_param['stride'], conv_param['pad']
-
-    # Check dimensions
-    assert (W + 2 * pad - filter_width) % stride == 0, 'width does not work'
-    assert (H + 2 * pad - filter_height) % stride == 0, 'height does not work'
-
-    # Create output
-    out_height = (H + 2 * pad - filter_height) // stride + 1
-    out_width = (W + 2 * pad - filter_width) // stride + 1
-    out = np.zeros((N, num_filters, out_height, out_width), dtype=x.dtype)
-
-    x_cols = im2col_indices(x, w.shape[2], w.shape[3], pad, stride)
-    # x_cols = im2col_cython(x, w.shape[2], w.shape[3], pad, stride)
-    res = w.reshape((w.shape[0], -1)).dot(x_cols) + b.reshape(-1, 1)
-
-    out = res.reshape(w.shape[0], out.shape[2], out.shape[3], x.shape[0])
-    out = out.transpose(3, 0, 1, 2)
-
-    cache = (x, w, b, conv_param, x_cols)
+    out = None
+    pad_num = conv_param['pad']
+    stride = conv_param['stride']
+    N,C,H,W = x.shape
+    F,C,HH,WW = w.shape
+    H_prime = (H+2*pad_num-HH) // stride + 1
+    W_prime = (W+2*pad_num-WW) // stride + 1
+    out = np.zeros([N,F,H_prime,W_prime])
+    #im2col
+    for im_num in range(N):
+        im = x[im_num,:,:,:]
+        im_pad = np.pad(im,((0,0),(pad_num,pad_num),(pad_num,pad_num)),'constant')
+        im_col = im2col(im_pad,HH,WW,stride)
+        filter_col = np.reshape(w,(F,-1))
+        mul = im_col.dot(filter_col.T) + b
+        out[im_num,:,:,:] = col2im(mul,H_prime,W_prime,1)
+    cache = (x, w, b, conv_param)
     return out, cache
 
-
-def conv_forward_strides(x, w, b, conv_param):
-    N, C, H, W = x.shape
-    F, _, HH, WW = w.shape
-    stride, pad = conv_param['stride'], conv_param['pad']
-
-    # Check dimensions
-    #assert (W + 2 * pad - WW) % stride == 0, 'width does not work'
-    #assert (H + 2 * pad - HH) % stride == 0, 'height does not work'
-
-    # Pad the input
-    p = pad
-    x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
-
-    # Figure out output dimensions
-    H += 2 * pad
-    W += 2 * pad
-    out_h = (H - HH) // stride + 1
-    out_w = (W - WW) // stride + 1
-
-    # Perform an im2col operation by picking clever strides
-    shape = (C, HH, WW, N, out_h, out_w)
-    strides = (H * W, W, 1, C * H * W, stride * W, stride)
-    strides = x.itemsize * np.array(strides)
-    x_stride = np.lib.stride_tricks.as_strided(x_padded,
-                  shape=shape, strides=strides)
-    x_cols = np.ascontiguousarray(x_stride)
-    x_cols.shape = (C * HH * WW, N * out_h * out_w)
-
-    # Now all our convolutions are a big matrix multiply
-    res = w.reshape(F, -1).dot(x_cols) + b.reshape(-1, 1)
-
-    # Reshape the output
-    res.shape = (F, N, out_h, out_w)
-    out = res.transpose(1, 0, 2, 3)
-
-    # Be nice and return a contiguous array
-    # The old version of conv_forward_fast doesn't do this, so for a fair
-    # comparison we won't either
-    out = np.ascontiguousarray(out)
-
-    cache = (x, w, b, conv_param, x_cols)
-    return out, cache
-
-
-def conv_backward_strides(dout, cache):
-    x, w, b, conv_param, x_cols = cache
-    stride, pad = conv_param['stride'], conv_param['pad']
-
-    N, C, H, W = x.shape
-    F, _, HH, WW = w.shape
-    _, _, out_h, out_w = dout.shape
-
-    db = np.sum(dout, axis=(0, 2, 3))
-
-    dout_reshaped = dout.transpose(1, 0, 2, 3).reshape(F, -1)
-    dw = dout_reshaped.dot(x_cols.T).reshape(w.shape)
-
-    dx_cols = w.reshape(F, -1).T.dot(dout_reshaped)
-    dx_cols.shape = (C, HH, WW, N, out_h, out_w)
-    # dx = col2im_6d_cython(dx_cols, N, C, H, W, HH, WW, pad, stride)
-
-    dx = col2im_indices(dx_cols, N, C, H, W, HH, WW, pad, stride)
-
-    return dx, dw, db
-
-
-def conv_backward_im2col(dout, cache):
+def conv_backward_fast(dout, cache):
     """
     A fast implementation of the backward pass for a convolutional layer
     based on im2col and col2im.
     """
-    x, w, b, conv_param, x_cols = cache
-    stride, pad = conv_param['stride'], conv_param['pad']
+    dx, dw, db = None, None, None
 
-    db = np.sum(dout, axis=(0, 2, 3))
+    x, w, b, conv_param = cache
+    pad_num = conv_param['pad']
+    stride = conv_param['stride']
+    N,C,H,W = x.shape
+    F,C,HH,WW = w.shape
+    H_prime = (H+2*pad_num-HH) // stride + 1
+    W_prime = (W+2*pad_num-WW) // stride + 1
 
-    num_filters, _, filter_height, filter_width = w.shape
-    dout_reshaped = dout.transpose(1, 2, 3, 0).reshape(num_filters, -1)
-    dw = dout_reshaped.dot(x_cols.T).reshape(w.shape)
+    dw = np.zeros(w.shape)
+    dx = np.zeros(x.shape)
+    db = np.zeros(b.shape)
 
-    dx_cols = w.reshape(num_filters, -1).T.dot(dout_reshaped)
-    dx = col2im_indices(dx_cols, x.shape, filter_height, filter_width, pad, stride)
-    # dx = col2im_cython(dx_cols, x.shape[0], x.shape[1], x.shape[2], x.shape[3],
-    #                    filter_height, filter_width, pad, stride)
+    # We could calculate the bias by just summing over the right dimensions
+    # Bias gradient (Sum on dout dimensions (batch, rows, cols)
+    #db = np.sum(dout, axis=(0, 2, 3))
 
+    for i in range(N):
+        im = x[i,:,:,:]
+        im_pad = np.pad(im,((0,0),(pad_num,pad_num),(pad_num,pad_num)),'constant')
+        im_col = im2col(im_pad,HH,WW,stride)
+        filter_col = np.reshape(w,(F,-1)).T
+
+        dout_i = dout[i,:,:,:]
+        dbias_sum = np.reshape(dout_i,(F,-1))
+        dbias_sum = dbias_sum.T
+
+        #bias_sum = mul + b
+        db += np.sum(dbias_sum,axis=0)
+        dmul = dbias_sum
+
+        #mul = im_col * filter_col
+        dfilter_col = (im_col.T).dot(dmul)
+        dim_col = dmul.dot(filter_col.T)
+
+        dx_padded = col2im_back(dim_col,H_prime,W_prime,stride,HH,WW,C)
+        dx[i,:,:,:] = dx_padded[:,pad_num:H+pad_num,pad_num:W+pad_num]
+        dw += np.reshape(dfilter_col.T,(F,C,HH,WW))
     return dx, dw, db
-
-
-conv_forward_fast = conv_forward_im2col
-conv_backward_fast = conv_backward_im2col
 
 
 def max_pool_forward_fast(x, pool_param):
